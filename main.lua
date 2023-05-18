96be6110f50a2d6a8d19f68cdfbb19e1699a2cd5
Ben Baker <BenBaker1997@gmail.com>
2023-05-18 16:03:19 -0700

    added roundOver, and print out winner

--- main.lua
+++ main.lua
@@ -17,20 +17,37 @@ function love.load()
     dealerHand = {}
     takeCard(dealerHand)
     takeCard(dealerHand)
+
+    roundOver = false
 end
 
 function love.keypressed(key)
-    if key == 'h' then
+    if key == 'h' and not roundOver then
         takeCard(playerHand)
+    elseif key == 's' then
+        roundOver = true
     end
 end
 
 function love.draw()
     local function getTotal(hand)
         local total = 0
+        local hasAce = false
 
         for cardIndex, card in ipairs(hand) do
-            total = total + card.rank
+            if card.rank > 10 then
+                total = total + 10
+            else
+                total = total + card.rank
+            end
+
+            if card.rank == 1 then 
+                hasAce = true
+            end
+        end
+
+        if hasAce and total < 12 then
+            total = total + 10
         end
 
         return total
@@ -50,7 +67,18 @@ function love.draw()
     for cardIndex, card in ipairs(dealerHand) do
         table.insert(output, 'suit: '..card.suit..', rank: '..card.rank)
     end
-    table.insert(output, 'Total: '..getTotal(dealerHand))
-    
-    love.graphics.print(table.concat(output, '\n'), 15, 15)
+    table.insert(output, 'Total: '..getTotal(dealerHand))       
+
+    if roundOver then
+        table.insert(output, '')
+
+        if getTotal(playerHand) > getTotal(dealerHand) then
+            table.insert(output, 'Player Wins!')
+        elseif getTotal(dealerHand) > getTotal(playerHand) then
+            table.insert(output, 'Dealer Wins!')
+        else 
+            table.insert(output, "Draw!")
+        end
+
+        love.graphics.print(table.concat(output, '\n'), 15, 15)
 end
