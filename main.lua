4ed72e02030edf689b4e773483066bd447efd6a2
Ben Baker <BenBaker1997@gmail.com>
2023-05-18 22:58:47 -0700

    Cards display suit and rank

--- main.lua
+++ main.lua
@@ -141,25 +141,28 @@ function love.draw()
     local function displayCard(card, x, y)
         love.graphics.setColor(1, 1, 1)
         love.graphics.draw(Images.card, x, y)
-
         love.graphics.setColor(0, 0, 0)
+
         local cardWidth = 53
         local cardHeight = 73
-        local numberOffsetX = 3
-        local numberOffsetY = 4
 
-        love.graphics.draw(
-            Images[card.rank],
-            x + numberOffsetX,
-            y + numberOffsetY
-        )
-        love.graphics.draw(
-            Images[card.rank],
-            x + cardWidth - numberOffsetX,
-            y + cardHeight - numberOffsetY,
-            0,
-            -1
-        )
+        local function drawCorner(image, offsetX, offsetY)
+            love.graphics.draw(
+                image,
+                x + offsetX,
+                y + offsetY
+            )
+            love.graphics.draw(
+                image,
+                x + cardWidth - offsetX,
+                y + cardHeight - offsetY,
+                0,
+                -1
+            )
+        end
+
+        drawCorner(Images[card.rank], 3, 4)
+        drawCorner(Images['mini_'..card.suit], 3, 14)
     end
 
     local testHand1 = {
